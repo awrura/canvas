b8ec119c83b96e8341086390d89d6e0d913f9dd5
twoics <second_vozhzhov@mail.ru>
2024-10-30 08:55:27 +0700

    feat: Добавил UI для рисования на матрице

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import MatrixGrid from './MatrixGrid'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar className="bg-body-tertiary mb-4">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://avatars.githubusercontent.com/u/166029138?s=400&u=82feebfa4f6744dc35aada15971814d037d1ad01&v=4"
+              width="35"
+              height="35"
+              className="d-inline-block align-top"
+            />{' '}
+            Awrura
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <MatrixGrid />
     </div>
   );
 }
---
+++ src/MatrixGrid.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { SketchPicker } from 'react-color';
+import './MatrixGrid.css'
+
+const ColorGrid = () => {
+  const [matrix, setMatrix] = useState(Array.from({ length: 16 }, () => Array(16).fill('#000000')));
+  const [color, setColor] = useState('#ff0000');
+  const [mousePressed, setMousePressed] = useState(false);
+
+  const handleMouseDown = () => setMousePressed(true);
+  const handleMouseUp = () => setMousePressed(false);
+
+  const toggleCellColor = (rowIndex, colIndex) => {
+    setMatrix((prevMatrix) =>
+      prevMatrix.map((row, rIndex) =>
+        row.map((cellColor, cIndex) => (rIndex === rowIndex && cIndex === colIndex) ? (cellColor === color ? null : color) : cellColor)
+      )
+    );
+  };
+
+  const handleCellClick = (rowIndex, colIndex) => {
+    if (!mousePressed) toggleCellColor(rowIndex, colIndex);
+  };
+
+  const handleCellMouseEnter = (rowIndex, colIndex) => {
+    if (mousePressed) {
+      setMatrix((prevMatrix) =>
+        prevMatrix.map((row, rIdx) =>
+          row.map((cellColor, cIdx) => ((rIdx === rowIndex && cIdx === colIndex) ? color : cellColor))
+        )
+      );
+    }
+  };
+
+  const handleColorChange = (colorResult) => setColor(colorResult.hex);
+
+  const handleSendClick = () => {
+    const rgbMatrix = matrix.map(row => row.map(cellColor => {
+      if (cellColor) {
+        const [r, g, b] = hexToRgb(cellColor);
+        return { red: r, green: g, blue: b };
+      }
+      return { red: 255, green: 255, blue: 255 };
+    }));
+    console.log(rgbMatrix);
+  };
+
+  const hexToRgb = (hex) => {
+    const bigint = parseInt(hex.slice(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return [r, g, b];
+  };
+  return (
+    <Container onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+      <div className="d-flex align-items-start">
+        <div className="grid-container mr-3">
+          {matrix.map((row, rowIndex) =>
+            row.map((cellColor, colIndex) => (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                className="square"
+                style={{ backgroundColor: cellColor || 'black' }}
+                onClick={() => handleCellClick(rowIndex, colIndex)}
+                onMouseEnter={() => handleCellMouseEnter(rowIndex, colIndex)}
+              >
+                <div></div>
+              </div>
+            ))
+          )}
+        </div>
+        <div className="mb-2 sketchpicker-container">
+          <SketchPicker className="mb-3" color={color} disableAlpha onChangeComplete={handleColorChange} />
+          <div className="d-flex justify-content-end mb-3">
+            <Button variant="primary" size="lg" onClick={handleSendClick}>Send</Button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default ColorGrid;

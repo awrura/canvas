7204371702434b7fe4430cc52e6102052d80d082
twoics <second_vozhzhov@mail.ru>
2024-11-09 09:51:09 +0700

    feat: Добавлен ввод для названия матрицы

--- src/MatrixGrid.js
+++ src/MatrixGrid.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, FormControl } from 'react-bootstrap';
 import { SketchPicker } from 'react-color';
 import './MatrixGrid.css';
 
@@ -7,6 +7,7 @@ const ColorGrid = () => {
   const [matrix, setMatrix] = useState(Array.from({ length: 16 }, () => Array(16).fill('#000000')));
   const [color, setColor] = useState('#ff0000');
   const [mousePressed, setMousePressed] = useState(false);
+  const [matrixName, setMatrixName] = useState('');
 
   const handleMouseDown = () => setMousePressed(true);
   const handleMouseUp = () => setMousePressed(false);
@@ -35,7 +36,13 @@ const ColorGrid = () => {
 
   const handleColorChange = (colorResult) => setColor(colorResult.hex);
 
+  const handleMatrixNameChange = (event) => setMatrixName(event.target.value);
+
   const handleSendClick = () => {
+    if (!matrixName) {
+      alert('Пожалуйста, введите имя матрицы.');
+      return;
+    }
 
     const rgbMatrix = matrix.flatMap(row => row.map(cellColor => {
       if (cellColor) {
@@ -45,8 +52,7 @@ const ColorGrid = () => {
       return { red: 255, green: 255, blue: 255 };
     }));
 
-    console.log(rgbMatrix);
-    fetch('matrix/rgb/hello_world', {
+    fetch(`matrix/rgb/${matrixName}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -72,26 +78,34 @@ const ColorGrid = () => {
 
   return (
     <Container onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-      <div className="d-flex align-items-start">
-        <div className="grid-container mr-3">
-          {matrix.map((row, rowIndex) =>
-            row.map((cellColor, colIndex) => (
-              <div
-                key={`${rowIndex}-${colIndex}`}
-                className="square"
-                style={{ backgroundColor: cellColor || 'black' }}
-                onClick={() => handleCellClick(rowIndex, colIndex)}
-                onMouseEnter={() => handleCellMouseEnter(rowIndex, colIndex)}
-              >
-                <div></div>
-              </div>
-            ))
-          )}
-        </div>
-        <div className="mb-2 sketchpicker-container">
-          <SketchPicker className="mb-3" color={color} disableAlpha onChangeComplete={handleColorChange} />
-          <div className="d-flex justify-content-end mb-3">
-            <Button variant="primary" size="lg" onClick={handleSendClick}>Send</Button>
+      <div className="d-flex align-items-start flex-column">
+        <FormControl
+          className="mb-3"
+          placeholder="Введите имя матрицы"
+          value={matrixName}
+          onChange={handleMatrixNameChange}
+        />
+        <div className="d-flex">
+          <div className="grid-container mr-3">
+            {matrix.map((row, rowIndex) =>
+              row.map((cellColor, colIndex) => (
+                <div
+                  key={`${rowIndex}-${colIndex}`}
+                  className="square"
+                  style={{ backgroundColor: cellColor || 'black' }}
+                  onClick={() => handleCellClick(rowIndex, colIndex)}
+                  onMouseEnter={() => handleCellMouseEnter(rowIndex, colIndex)}
+                >
+                  <div></div>
+                </div>
+              ))
+            )}
+          </div>
+          <div className="mb-2 sketchpicker-container">
+            <SketchPicker className="mb-3" color={color} disableAlpha onChangeComplete={handleColorChange} />
+            <div className="d-flex justify-content-end mb-3">
+              <Button variant="primary" size="lg" onClick={handleSendClick} disabled={!matrixName}>Send</Button>
+            </div>
           </div>
         </div>
       </div>
